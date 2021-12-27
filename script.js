59df82f07b03f2cfc9ce0c0893e61787624f26a7
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2021-12-27 17:05:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ const first = () => {
    document.getElementById("slide").src= "motive2.webp";
    }
    setInterval(second, 4000);
+
+ const third = () => {
+19	-
+20	- //     document.getElementById('slide').src= 'motive3.jpeg';
+21	- //   }
+22	- //   setInterval(third, 6000);
+
+
